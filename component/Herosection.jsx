6cc6f21612924f5e1224384698375a0a411c8739
jirawat rongtong <jirawat.guy.rongtong@gmail.com>
2023-01-30 22:09:23 +0700

    add style lefthero

--- component/Herosection.jsx
+++ component/Herosection.jsx
@@ -2,21 +2,34 @@ import React from "react";
 import styles from "../styles/Herosection.module.css";
 import ProfilePic from "../public/profile.jpg";
 import Image from "next/image";
-import { animations, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 function Herosection() {
   return (
     <div className={styles.containner}>
-      <div>
-        <h1>
-          Hi! I'm Jirawat <br /> Full Stack Developer
-        </h1>
-
-        <button>Contact</button>
+      <div className={styles.left_section}>
+        <h1>Hi! I'm Jirawat</h1>{" "}
+        <h1 className={styles.text_gradient}> Full-Stack Developer</h1>
+        <p> Eager to learn, driven to explore, constantly pushing boundaries</p>
+        <motion.div className={styles.button_gradient}></motion.div>
+        <motion.button whileHover={{ x: 5, y: 4 }} whileTap={{ scale: 0.9 }}>
+          Contact
+        </motion.button>
         <a>See Portfolio</a>
       </div>
       <div className={styles.right_section}>
-        <Image src={ProfilePic} />
+        <motion.div
+          className={styles.img_container}
+          initial={{ opacity: 0, x: 200 }}
+          animate={{
+            opacity: 1,
+            x: 0,
+          }}
+          transition={{ type: "spring", bounce: 0.6, duration: 3 }}
+        >
+          <Image src={ProfilePic} />
+        </motion.div>
+
         <motion.div
           className={styles.halfcircle}
           initial={{ rotate: 15, y: -200, opacity: 0 }}
